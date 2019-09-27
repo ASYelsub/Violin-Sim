a0855a499ee94a7f811b94f5da60855c62a94b93
Kirijo <asyelsub@gmail.com>
2019-09-27 15:47:14 -0400

    Initial commit

---
+++ Assets/HandMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//usage: move hand with mouse according to constraints
+public class HandMovement : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Transform myTransform;
+    Vector3 myInput;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //we want z transform to increase when pushing mouse up and to decrease when pushing mouse down
+        float mouseY = Input.GetAxis("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X");
+        myInput.y = 0f;
+        myInput.x = 0f;
+        myInput.z = mouseY;
+    }
+
+    void FixedUpdate(){
+      
+        
+    }
+}
