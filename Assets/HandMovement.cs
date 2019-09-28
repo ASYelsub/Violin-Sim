b2c40eb357a9bddea83a2d3595a402746b46c8a0
Kirijo <asyelsub@gmail.com>
2019-09-28 14:26:49 -0400

    Attempt to fix hand movement w physics, could work but need some help.

--- Assets/HandMovement.cs
+++ Assets/HandMovement.cs
@@ -12,6 +12,7 @@ public class HandMovement : MonoBehaviour
     {
 
         myTransform = GetComponent<Transform>();
+
     }
 
     void Update()
@@ -19,11 +20,13 @@ public class HandMovement : MonoBehaviour
         //we want z transform to increase when pushing mouse up and to decrease when pushing mouse down
         float mouseY = Input.GetAxis("Mouse Y");
         float mouseX = Input.GetAxis("Mouse X");
-        bow.transform.Translate(new Vector3(0f,0f,-mouseY*.5f));
-        myTransform.Translate(new Vector3(0f,0f,-mouseY*.5f));
+        Vector3 mouseInput = new Vector3(mouseX,mouseY,0f);
+        /*bow.transform.Translate(new Vector3(0f,0f,-mouseY*.5f));
+        
         lowerArm.transform.Rotate(new Vector3(0f,-mouseY*10f,0f));
         upperArm.transform.Rotate(new Vector3(0f,-mouseY*10f,0f));
-
+*/
+        myTransform.Translate(0f,0f,-mouseY*.25f);
     
          
 
