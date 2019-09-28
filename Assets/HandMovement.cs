07b58b5a5e8890c10edd4f49189f22131566372e
Kirijo <asyelsub@gmail.com>
2019-09-28 16:23:53 -0400

    I got arm movement to work but I have no constraints yet so it can hyperextend and get fucked up! Excited tho!

--- Assets/HandMovement.cs
+++ Assets/HandMovement.cs
@@ -18,15 +18,21 @@ public class HandMovement : MonoBehaviour
     void Update()
     {
         //we want z transform to increase when pushing mouse up and to decrease when pushing mouse down
+        
         float mouseY = Input.GetAxis("Mouse Y");
         float mouseX = Input.GetAxis("Mouse X");
-        Vector3 mouseInput = new Vector3(mouseX,mouseY,0f);
+        //mouseY = Mathf.Clamp(mouseY, -1f, .5f);
+        Vector3 mouseInput = new Vector3(0f,0f,-mouseY);
+        myTransform.Translate(mouseInput);
         /*bow.transform.Translate(new Vector3(0f,0f,-mouseY*.5f));
         
         lowerArm.transform.Rotate(new Vector3(0f,-mouseY*10f,0f));
         upperArm.transform.Rotate(new Vector3(0f,-mouseY*10f,0f));
 */
-        myTransform.Translate(0f,0f,-mouseY*.25f);
+        
+        
+        
+        
     
          
 
