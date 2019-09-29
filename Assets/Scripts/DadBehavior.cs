4b57094b9d3057ba5eaa69c9113774de0d470b2e
Kirijo <asyelsub@gmail.com>
2019-09-29 17:06:18 -0400

    Adding the first experience/time based bars, no code yet

---
+++ Assets/Scripts/DadBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DadBehavior : MonoBehaviour
+{
+
+    public GameObject patienceBar;
+    public Transform patienceBarTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
