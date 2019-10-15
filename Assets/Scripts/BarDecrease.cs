827d584b2964b95e5a3ec6e21bdf24f37a1fb1e4
Kirijo <asyelsub@gmail.com>
2019-10-15 13:40:14 -0400

    Playable build except UI for gameplay scene doesn't show up when built to WebGL

--- Assets/Scripts/BarDecrease.cs
+++ Assets/Scripts/BarDecrease.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BarDecrease : MonoBehaviour
 {
@@ -10,11 +11,16 @@ public class BarDecrease : MonoBehaviour
     float currentTime = 30.0f;
     public void SheDecreases()
     {
-            currentTime -= Time.deltaTime;
+            currentTime -= Time.deltaTime*5;
  
             float ratio = currentTime / fullTime;
  
             Vector3 currentScale = transform.localScale;
             transform.localScale = new Vector3(Mathf.Clamp(ratio, 0f, 6f), currentScale.y, currentScale.z);           
+    }
+    void Update(){
+        if (transform.localScale.x <= 0f){
+            SceneManager.LoadScene(2);
+        }
     }
 }
